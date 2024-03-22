26c7cf0a6b740ad9f6e9e00d55568368d7c7a417
Saikat Kar <ksaikat009@gmail.com>
2024-03-22 15:48:58 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index'); 
@@ -8,6 +9,7 @@ const setupAndStartServer = async()=>{
 
     const app=express();
 
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
